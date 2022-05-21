3980f515df73f88c3ac8a26606db81bdfc04b43c
TWcamel <j0930495159@gmail.com>
2022-05-21 18:25:52 +0800

    update docker scripts

--- server/deploy.sh
+++ server/deploy.sh
@@ -5,7 +5,8 @@ sudo docker build -t $IMAGE_NAME .
 sudo docker 2>/dev/null stop $CONTAINER_NAME | true 
 sudo docker 2>/dev/null rm $CONTAINER_NAME | true
 sudo docker run -d -p $PORT:$PORT --name $CONTAINER_NAME $IMAGE_NAME 
-sudo docker 2>/dev/null cp ./.env $CONTAINER_NAME:/app/.env | true
+sudo docker 2>/dev/null cp ./.env $CONTAINER_NAME:/app/server/.env | true
+sudo docker 2>/dev/null cp $CURRENT_DIR/client/build $CONTAINER_NAME:/app/client | true
 echo ".env file copied to container $CONTAINER_NAME on port $PORT"
 echo "Docker image $IMAGE_NAME is running on port $PORT"
 echo "To stop the container, run: sudo docker stop $(sudo docker ps -q)"
